78fa314a87c89cd7ba85a766faaa6225b7df1266
Izafatava Nastassia <nizafatava@exadel.net>
2023-02-22 18:11:18 +0300

    Initial commit

---
+++ nizafatava_python_lesson1.py
@@ -0,0 +1,34 @@
+# задание №1
+a = 4
+b = 6
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a ** b)
+print(a / abs(b))
+print(a // b)
+
+# задание №2
+
+x = (((17 / 2) * 3) + 2)
+print(x)
+
+x = (2 + ((17 / 2) * 3))
+print(x)
+
+x = ((19 % 4) + ((15 / 2) * 3))
+print(x)
+
+x = ((15 + 6) - (10 * 4))
+print(x)
+
+x = (((17 / 2) % 2) * (3 ** 3))
+print(x)
+
+# задание 3
+
+x = 2
+y = 3
+c = (abs(x) - abs(y))/(1 + abs(x * y))
+print(c)
